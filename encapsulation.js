7f46df3d906dc229181470d35f4f513dd0691c64
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2023-01-06 13:24:19 +0100

    private and protected fields update

--- encapsulation.js
+++ encapsulation.js
@@ -2,13 +2,24 @@ class Navigation {
     // # jako pole prywatne
     #L1 = 1602;
 
+    // _ jako pole chronione
+    _L2 = 3218;
+
     getDistanceTo(x) {
         const position = this._getCurrentPosition();
         return this._calculateDistanceToX(position, x, this.#L1);
     }
 }
 
-const carNavigation = new Navigation();
-
 // SyntaxError: Private field '#L1' must be declared in an enclosing class
-console.log(carNavigation.#L1);
+// console.log(carNavigation.#L1);
+
+class BikeNavigation extends Navigation {
+    getFieldValues() {
+        console.log(this._L2); // 3218
+        console.log(this.#L1); // Error Private field '#L1'
+    }
+}
+
+const bikeNavigation = new BikeNavigation();
+bikeNavigation.getFieldValues();
